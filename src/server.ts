5ca9e2e69a6e9a3d77e43c97f95694006628666e
leeyc924 <leeyc924@naver.com>
2021-08-01 13:00:15 +0900

    몽고 db 연결

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,17 @@ import { configureApp } from './app';
 
 const port = process.env.PORT || 8005;
 
-configureApp().listen(port, () => {
-  console.log('Express is listening on port', port);
-});
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const url = 'mongodb+srv://leeyc:dudcjf12@editor.xwhv9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+MongoClient.connect(url)
+  .then(client => {
+    console.log('mongo connected');
+    console.log(client);
+  })
+  .then(configureApp().listen(port, () => {
+    console.log('Express is listening on port', port);
+  }))
+  .catch(err => console.log(err));
+
