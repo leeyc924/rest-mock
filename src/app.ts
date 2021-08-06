3b14cbe964a874b12357e0d92cc93001dfb2d37e
leeyc924 <leeyc924@naver.com>
2021-08-06 21:46:32 +0900

    test

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import router from './routes/index';
 
 export function configureApp() {
   connectMongoDb();
-
+ 
   const app = asyncify(express());
   app.use(express.urlencoded({ limit: '30mb', extended: true, parameterLimit: 100000 }));
   app.use(express.json({ limit: '30mb' }));
