0f73bb7a54c14c0af06d232a63734e44731dfb5d
leeyc924 <leeyc924@naver.com>
2023-04-29 23:24:49 +0900

    add order router

--- src/router/cafe.ts
+++ src/router/cafe.ts
@@ -20,4 +20,16 @@ router.get('/menu', async (req, res) => {
   }
 });
 
+router.post('/order', async (req, res) => {
+  try {
+    const menuList = req.body['menuList'] || [];
+    if (menuList.length <= 0) {
+      throw new Error("no menu");
+    }
+    res.json(true);
+  } catch (error) {
+    res.status(400).json({ message: 'eror' });
+  }
+});
+
 export default router;
