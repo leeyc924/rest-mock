1cb091dac0231d2397f0fccd537406286d955c64
leeyc924 <leeyc924@naver.com>
2023-05-06 22:00:06 +0900

    Modify lambda test

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,27 @@
 process.env.TZ = 'Asia/Seoul';
 
-import serverlessExpress from '@vendia/serverless-express';
+import serverless from 'serverless-http';
 import configureApp from './app';
 
+const binaryMimeTypes: string[] = [
+  // 'application/javascript',
+  // 'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  // 'text/css',
+  // 'text/html',
+  // 'text/javascript',
+  // 'text/plain',
+  'text/text',
+  'text/xml',
+];
+
 const app = configureApp();
-export const handler = serverlessExpress({ app });
+export const handler = serverless(app);
